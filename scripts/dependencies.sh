abb08eba2b70bfd9d7b0108f30e3b1244466b173
AIBluefisher <1701213988@pku.edu.cn>
2021-04-19 08:27:08 +0800

    fix travis test

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -3,7 +3,14 @@
 sudo apt-get update
 
 sudo apt-get install git cmake build-essential \
-  libgoogle-glog-dev libgflags-dev libgtest-dev libeigen3-dev
+  libgoogle-glog-dev libgflags-dev libgtest-dev
+
+git clone https://github.com/eigenteam/eigen-git-mirror.git
+cd eigen-git-mirror
+git checkout 3.2.10
+mkdir build && cd build
+cmake .. && make
+sudo make install
 
 sudo apt-get install libatlas-base-dev libsuitesparse-dev
 git clone https://ceres-solver.googlesource.com/ceres-solver
