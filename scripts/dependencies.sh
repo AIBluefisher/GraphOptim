02e9110489ecd65d9292bc8853228caaaa00fe07
Chen Yu <chenyu@comp.nus.edu.sg>
2022-07-14 22:39:25 +0800

    [INFO] Support compilation with COLMAP

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -7,6 +7,7 @@ sudo apt-get install git cmake build-essential \
 
 mkdir tmp && cd tmp
 
+# Install Ceres-Solver
 sudo apt-get install libatlas-base-dev libsuitesparse-dev
 git clone https://ceres-solver.googlesource.com/ceres-solver
 cd ceres-solver
@@ -15,6 +16,38 @@ git checkout 1.14.0
 mkdir build && cd build
 cmake .. -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF
 make -j8
+sudo make install
+cd ../../
+
+# Install COLMAP
+sudo apt-get install \
+    git \
+    cmake \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libboost-test-dev \
+    libsuitesparse-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgflags-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev
+
+sudo apt-get install libcgal-qt5-dev
+
+git clone https://github.com/colmap/colmap.git
+cd colmap
+git checkout dev
+mkdir build
+cd build
+cmake ..
+make -j
 sudo make install
 cd ../../
 
